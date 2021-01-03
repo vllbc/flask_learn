f2ea5216061c0afbe97b956352a96d4813c8035f
vllbc <1683070754@qq.com>
2021-01-03 16:23:45 +0800

    add template

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for,escape
+from flask import Flask,url_for,escape,render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'hello world! <img src="http://helloflask.com/totoro.gif">'
+    movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+    return render_template("index.html",name='wlb',movies=movies)
 
 @app.route('/user/<name>')
 def user(name):
