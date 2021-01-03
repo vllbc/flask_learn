5bac79b7fa5cc6e8b71c5c5992d26a10e33e289d
vllbc <1683070754@qq.com>
2021-01-03 16:14:27 +0800

    add homepage and user

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask,url_for,escape
+
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def run():
-    return 'hello world!'
+def hello():
+    return 'hello world! <img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>HELLO {escape(name)}!</h1>"
+
+
+@app.route('/test')
+def test_for_user():
+    print(url_for("hello"))
+    print(url_for('user',name='wlb'))
+    print(url_for("test_for_user"))
+    return 'Testpage'
 app.run()
