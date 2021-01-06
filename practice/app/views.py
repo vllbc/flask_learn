36966d48c661e205aa733ba8b7bd7c451f84b21e
vllbc <1683070754@qq.com>
2021-01-06 15:06:14 +0800

    a simple edit

--- practice/app/views.py
+++ practice/app/views.py
@@ -1,6 +1,7 @@
+
 from app import app,db
 
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,flash
 
 
 @app.route("/",methods=['GET','POST'])
@@ -8,6 +9,9 @@ def index():
     from app.models import SayModel
     if request.method == 'POST':
         data = request.form
+        if len(data['name']) > 20 or len(data['body']) > 200:
+            flash("error！")
+            return redirect(url_for("index"))
         say = SayModel(name=data['name'],body=data['body'])
         db.session.add(say)
         db.session.commit()
@@ -15,4 +19,24 @@ def index():
     says = SayModel.query.all()    
     return render_template("index.html",says=says)
 
+@app.route("/delete/<int:sayid>",methods=['GET','POST'])
+def delete(sayid):
+    from app.models import SayModel
+    say = SayModel.query.get_or_404(sayid)
+    db.session.delete(say)
+    db.session.commit()
+    return redirect(url_for("index"))
 
+
+@app.route('/edit/<int:sayid>',methods=['GET','POST'])
+def edit(sayid):
+    from app.models import SayModel
+    if request.method == 'POST':
+        data = request.form
+        say = SayModel.query.get_or_404(sayid)
+        say.name = data['name']
+        say.body = data['body']
+        db.session.commit()
+        flash("edit ok")
+        return redirect(url_for("index"))
+    return render_template("edit.html")
